038698887ffab179d0526cbfe8a152afc62b9d71
Axel Draws <axel.draws@outlook.com>
2023-05-01 14:02:50 +0000

    Added exams and data

---
+++ backend/src/api/exam/controllers/exam.ts
@@ -0,0 +1,7 @@
+/**
+ * exam controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::exam.exam');
---
+++ backend/src/api/exam/services/exam.ts
@@ -0,0 +1,7 @@
+/**
+ * exam service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::exam.exam');
