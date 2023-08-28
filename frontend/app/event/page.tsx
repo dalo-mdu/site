1657fb2b926f10cf14176a51dbf1a6fe7b2901a9
Axel Draws <axel.draws@outlook.com>
2023-08-28 16:08:35 +0200

    Added filter to remove events that have passed

--- frontend/app/event/page.tsx
+++ frontend/app/event/page.tsx
@@ -2,14 +2,19 @@ import EventPreview from '@/components/event.component'
 import client from '@/graphql/client';
 import { LIST_EVENT } from '@/graphql/queries';
 import { IEventAttributes, IEventInfo } from '@/types/types';
-import Head from 'next/head';
-import Image from 'next/image'
-import Link from 'next/link'
 
 
 export default async function Event() {
-    const { data: eventData } = await client.query({ query: LIST_EVENT })
-    const events: IEventAttributes[] = eventData.events.data;
+    const { data: eventData }:{data:{events:{data:IEventAttributes[]}}} = await client.query({ query: LIST_EVENT })
+    const events: IEventAttributes[] = eventData.events.data.filter((event) => {
+      let now = new Date();
+      now.setHours(0,0,0,0);
+      let eventDate = new Date(event.attributes.info.date)
+      eventDate.setHours(0,0,0,0);
+      return eventDate >= now;
+    }).sort((a, b) => {
+      return new Date(a.attributes.info.date).getTime() - new Date(b.attributes.info.date).getTime()
+    });
     return (
         <div className="flex flex-col gap-2 w-full 2xl:w-1/2 mx-auto  p-4 items-center  rounded-lg pt-5">
             {
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -11,8 +11,16 @@ import Link from 'next/link'
 
 
 export default async function Home() {
-  const { data: eventData } = await client.query({ query: LIST_EVENT })
-  const events: IEventAttributes[] = eventData.events.data;
+  const { data: eventData }:{data:{events:{data:IEventAttributes[]}}} = await client.query({ query: LIST_EVENT })
+  const events: IEventAttributes[] = eventData.events.data.filter((event) => {
+    let now = new Date();
+    now.setHours(0,0,0,0);
+    let eventDate = new Date(event.attributes.info.date)
+    eventDate.setHours(0,0,0,0);
+    return eventDate >= now;
+  }).sort((a, b) => {
+    return new Date(a.attributes.info.date).getTime() - new Date(b.attributes.info.date).getTime()
+  });
 
   //const { data: newsData } = await client.query({ query: LIST_NEW })
   //const news: INewAttributes[] = newsData.news.data;
